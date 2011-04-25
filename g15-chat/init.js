75091aa1a84c6aae10225e42c49f5038fd908677
Jules Blok <jules.blok@gmail.com>
2011-04-25 14:12:01 +0000

    * Now filters colors/bold as they are not printable on the LCD display, though I might add bold later * No longer displays username if it's the same user as the last message
    git-svn-id: http://jules.aerix.nl/svn/g15-chat/trunk@15 0c6869d3-5502-4444-ba6c-6edb0ead1ba3

--- g15-chat/init.js
+++ g15-chat/init.js
@@ -5,74 +5,82 @@ var LcdInit;
 var LcdClose;
 var LcdPrint;
 
+var ColorFilter = new RegExp("\\x1f|\\x02|\\x12|\\x0f|\\x16|\\x03(?:\\d{1,2}(?:,\\d{1,2})?)?", "g");
+var LastUser;
+var LastPrivateUser;
+
 plugin.init =
 function _init(glob) {
-	this.major = 0;  // Major version number.
-	this.minor = 5;  // Minor version number.
-	this.version = this.major + "." + this.minor;
-	this.description = "ChatZilla G15 Display Plugin";
-	
-	return true;
+    this.major = 0;  // Major version number.
+    this.minor = 5;  // Minor version number.
+    this.version = this.major + "." + this.minor;
+    this.description = "ChatZilla G15 Display Plugin";
+
+    return true;
 }
 
 plugin.enable =
 function _enable() {
-	// Open the G15-Chat library
-	Components.utils.import("resource://gre/modules/ctypes.jsm");
-	LcdLib = ctypes.open(decodeURI(plugin.cwd.substr(8)) + "g15-chat.dll"); //Substr to remove file:///
-	LcdInit = LcdLib.declare("LcdInit", ctypes.default_abi, ctypes.int, ctypes.jschar.ptr); // Returns: int Params: w_char*
-	LcdClose = LcdLib.declare("LcdClose", ctypes.default_abi, ctypes.void_t); // Returns: void Params: void
-	LcdPrint = LcdLib.declare("LcdPrint", ctypes.default_abi, ctypes.void_t, ctypes.jschar.ptr); // Returns: void Params: w_char*
-	
-	// Connect to the LCD display
-	var ret = LcdInit("ChatZilla");
-	if(ret != 0) return false; // Failed to connect
-	
-	// Add our message hooks
-	client.eventPump.addHook([{set:"channel", type:"privmsg"}], hookMessage, this.id + "-hook-message");
-	client.eventPump.addHook([{set:"user", type:"privmsg"}], hookPrivate, this.id + "-hook-private");
-	client.eventPump.addHook([{set:"channel", type:"action"}], hookAction, this.id + "-hook-action");
-	client.eventPump.addHook([{set:"user", type:"action"}], hookAction, this.id + "-hook-privaction");
-	
-	return true;
+    // Open the G15-Chat library
+    Components.utils.import("resource://gre/modules/ctypes.jsm");
+    LcdLib = ctypes.open(decodeURI(plugin.cwd.substr(8)) + "g15-chat.dll"); //Substr to remove file:///
+    LcdInit = LcdLib.declare("LcdInit", ctypes.default_abi, ctypes.int, ctypes.jschar.ptr); // Returns: int Params: wchar_t*
+    LcdClose = LcdLib.declare("LcdClose", ctypes.default_abi, ctypes.void_t); // Returns: void Params: void
+    LcdPrint = LcdLib.declare("LcdPrint", ctypes.default_abi, ctypes.void_t, ctypes.jschar.ptr); // Returns: void Params: wchar_t*
+
+    // Connect to the LCD display
+    var ret = LcdInit("ChatZilla");
+    if (ret != 0) return false; // Failed to connect
+
+    // Add our message hooks
+    client.eventPump.addHook([{ set: "channel", type: "privmsg"}], hookMessage, this.id + "-hook-message");
+    client.eventPump.addHook([{ set: "user", type: "privmsg"}], hookPrivate, this.id + "-hook-private");
+    client.eventPump.addHook([{ set: "channel", type: "action"}], hookAction, this.id + "-hook-action");
+    client.eventPump.addHook([{ set: "user", type: "action"}], hookAction, this.id + "-hook-privaction");
+
+    return true;
 }
 
 plugin.disable =
 function _disable() {
-	// Remove our message hooks
-	client.eventPump.removeHookByName(this.id + "-hook-message");
-	client.eventPump.removeHookByName(this.id + "-hook-private");
-	client.eventPump.removeHookByName(this.id + "-hook-action");
-	client.eventPump.removeHookByName(this.id + "-hook-privaction");
-	
-	// Close the connection and the library
-	// If these functions aren't called firefox will lock up
-	LcdClose();
-	LcdLib.close();
-	
-	return true;
+    // Remove our message hooks
+    client.eventPump.removeHookByName(this.id + "-hook-message");
+    client.eventPump.removeHookByName(this.id + "-hook-private");
+    client.eventPump.removeHookByName(this.id + "-hook-action");
+    client.eventPump.removeHookByName(this.id + "-hook-privaction");
+
+    // Close the connection and the library
+    // If these functions aren't called firefox will lock up
+    LcdClose();
+    LcdLib.close();
+
+    return true;
 }
 
 function hookMessage(e) {
-	try {
-		LcdPrint("<" + e.user.unicodeName + ">" + e.msg);
-	} catch(ex) {
-		// We should never let exceptions get out of a hook, or bad things happen.
-	}
+    try {
+        if (LastUser == e.user) LcdPrint(e.msg.replace(ColorFilter, ""));
+        else LcdPrint("<" + e.user.unicodeName.replace(ColorFilter, "") + ">" + e.msg.replace(ColorFilter, ""));
+        LastUser = e.user;
+    } catch (ex) {
+        // We should never let exceptions get out of a hook, or bad things happen.
+    }
 }
 
 function hookAction(e) {
-	try {
-		LcdPrint(e.user.unicodeName + " " + e.CTCPData);
-	} catch(ex) {
-		// We should never let exceptions get out of a hook, or bad things happen.
-	}
+    try {
+        LcdPrint(e.user.unicodeName.replace(ColorFilter, "") + " " + e.CTCPData.replace(ColorFilter, ""));
+    } catch (ex) {
+        // We should never let exceptions get out of a hook, or bad things happen.
+    }
 }
 
 function hookPrivate(e) {
-	try {
-		LcdPrint("{" + e.user.unicodeName + "}" + e.msg);
-	} catch(ex) {
-		// We should never let exceptions get out of a hook, or bad things happen.
-	}
+    try {
+        if (LastPrivateUser == e.user) LcdPrint(e.msg.replace(ColorFilter, ""));
+        else LcdPrint("{" + e.user.unicodeName.replace(ColorFilter, "") + "}" + e.msg.replace(ColorFilter, ""));
+        LastPrivateUser = e.user;
+    } catch (ex) {
+        // We should never let exceptions get out of a hook, or bad things happen.
+    }
 }
