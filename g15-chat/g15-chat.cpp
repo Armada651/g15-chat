048c7ebd34dd3391251bfbf2821c99b8aab25c9a
Jules Blok <jules.blok@gmail.com>
2011-05-16 20:54:23 +0000

    Started implementation of horizontal scrolling
    git-svn-id: http://jules.aerix.nl/svn/g15-chat/trunk@31 0c6869d3-5502-4444-ba6c-6edb0ead1ba3

--- g15-chat/g15-chat.cpp
+++ g15-chat/g15-chat.cpp
@@ -17,6 +17,26 @@ CLCDText Lines[LINES]; // The text lines on the page
 wchar_t* Strings[LINES]; // Buffer with strings printed
 unsigned char FirstLine; // Indicates the newest line in the buffer
 HANDLE hMutex; // Thread-safety mutex
+unsigned int Offset;
+
+DWORD CALLBACK OnLCDButtonsCallback(int device, DWORD dwButtons, const PVOID pContext)
+{
+	switch(dwButtons)
+	{
+		case LGLCDBUTTON_BUTTON0: 
+			{
+				if(Offset>0) Offset-=10;
+			}
+		break;
+		case LGLCDBUTTON_BUTTON1:
+			{
+				Offset+=10;
+			}
+		break;
+	}
+	LcdDraw();
+	return 0;
+}
 
 int LcdInit( wchar_t* name )
 {
@@ -27,8 +47,9 @@ int LcdInit( wchar_t* name )
 	hMutex = CreateMutex(NULL, true, NULL);
 	if(hMutex == NULL) return 3;
 
-	// Set up the connection context
+	// Set up the connection and softbutton context
     lgLcdConnectContextEx ConnectCtx;
+	lgLcdSoftbuttonsChangedContext ButtonCtx;
 
 	ConnectCtx.appFriendlyName = name;
 	ConnectCtx.dwAppletCapabilitiesSupported = LGLCD_APPLET_CAP_BW;
@@ -39,8 +60,10 @@ int LcdInit( wchar_t* name )
     ConnectCtx.onNotify.notificationCallback = NULL;
     ConnectCtx.onNotify.notifyContext = NULL;
 
+	ButtonCtx.softbuttonsChangedCallback = OnLCDButtonsCallback;
+
 	// Initialize the connection
-	if(Connection.Initialize(ConnectCtx) == FALSE)
+	if(Connection.Initialize(ConnectCtx, &ButtonCtx) == FALSE)
 	{
 		// Destroy the mutex
 		CloseHandle(hMutex);
@@ -52,6 +75,7 @@ int LcdInit( wchar_t* name )
 	CLCDOutput* monoOutput = Connection.MonoOutput();
 
 	// Initialize the text lines and add them to the page
+	Offset = 0;
 	FirstLine = 0;
 	memset(Strings, NULL, sizeof(Strings));
 	for(int i=0; i<LINES; i++)
@@ -113,6 +137,21 @@ int LcdPrint( wchar_t* text )
 	Strings[FirstLine] = (wchar_t*)malloc(sizeof(wchar_t) * size);
 	wcscpy_s(Strings[FirstLine], size, text);
 	
+	LcdDraw();
+
+	// Release the mutex
+	ReleaseMutex(hMutex);
+	
+	return 0;
+}
+
+int LcdDraw( void )
+{
+	// Check if connected
+    if(!Connection.IsConnected()) return 1;
+
+	//TODO: Make thread-safe
+
 	// Set the text lines from oldest (top) to newest (bottom)
 	for(int i=LINES-1, j=FirstLine; i>=0; i--, j--)
 	{
@@ -120,14 +159,16 @@ int LcdPrint( wchar_t* text )
 		if(j<0) j=LINES-1;
 
 		// Set the line
-		Lines[i].SetText(Strings[j]);
+		if(Strings[j] != NULL)
+		{
+			unsigned int len = wcslen(Strings[j]);
+			if(Offset < len) Lines[i].SetText(Strings[j]+Offset);
+			else Lines[i].SetText(L"");
+		}
 	}
 
 	// Update the display
 	Connection.Update();
-
-	// Release the mutex
-	ReleaseMutex(hMutex);
 
 	return 0;
 }
--- g15-chat/g15-chat.h
+++ g15-chat/g15-chat.h
@@ -2,4 +2,5 @@ extern "C" {
 	int __declspec(dllexport) LcdInit( wchar_t* name );
 	int __declspec(dllexport) LcdClose( void );
 	int __declspec(dllexport) LcdPrint( wchar_t* text );
+	int __declspec(dllexport) LcdDraw( void );
 }
